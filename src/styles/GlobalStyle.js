4f00a1ce0f71e5eef96a535d88bd35978a8a96dd
juicylevel <rykov87yuri@gmail.com>
2019-12-08 21:35:45 +0300

    added fonts

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,7 +2,10 @@ import { createGlobalStyle } from 'styled-components';
 import backgroundImg from 'images/background.jpg';
 
 export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Roboto');
+
     body {
+        font-family: 'Roboto', sans-serif;
         background-image: url(${backgroundImg});
     }
 `;
--- src/views/App/App.js
+++ src/views/App/App.js
@@ -5,6 +5,7 @@ export default () => {
     return (
         <div>
             <GlobalStyle />
+            <h1 style={{ color: 'white' }}>Coffee!</h1>
         </div>
     );
 };
