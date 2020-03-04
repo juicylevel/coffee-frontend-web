768d5d99a1d3fbbe072ece76d93500840f89bfa3
rykov-yuri <rykov87yuri@gmail.com>
2020-03-04 10:24:56 +0300

    refactoring of form updating phone

---
+++ src/views/UpdatePhone/Form.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import { useForm, useField } from 'react-final-form-hooks';
+import { FieldAdaptor } from 'components/form/utils';
+import { PhoneField } from 'components/form/fields';
+import { required, phone, composeValidators } from 'components/form/validation';
+
+const validate = currentPhone => values => {
+    const errors = {};
+    const { phone, newPhone } = values;
+
+    if (phone !== currentPhone) {
+        errors.phone = 'Введён неправильный номер телефона';
+    } else if (newPhone === phone) {
+        errors.newPhone = 'Введён старый номер телефона';
+    }
+
+    return errors;
+};
+
+const Form = ({ 
+    currentPhone, 
+    onSubmit 
+}) => {
+    const { form, handleSubmit, submitting } = useForm({ 
+        onSubmit,
+        validate: validate(currentPhone),
+    });
+
+    const phoneFieldProps = useField(
+        'phone', 
+        form, 
+        composeValidators(required, phone)
+    );
+
+    const newPhoneFieldProps = useField(
+        'newPhone', 
+        form, 
+        composeValidators(required, phone)
+    );
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <Grid 
+                container 
+                direction="column"
+                spacing={3}
+            >
+                <Grid item xs={12}>
+                    <FieldAdaptor
+                        {...phoneFieldProps}
+                        component={PhoneField}
+                        variant="outlined"
+                        label="Текущий номер телефона"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <FieldAdaptor
+                        {...newPhoneFieldProps}
+                        component={PhoneField}
+                        variant="outlined"
+                        label="Новый номер телефона"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button 
+                        color="primary"
+                        variant="contained"
+                        fullWidth
+                        size="large"
+                        type="submit"
+                        disabled={submitting}
+                    >
+                        Сохранить
+                    </Button>
+                </Grid>
+            </Grid>
+        </form>
+    );
+};
+
+Form.propTypes = {
+    currentPhone: PropTypes.string,
+    onSubmit: PropTypes.func,
+};
+
+export default Form;
--- src/views/UpdatePhone/UpdatePhone.js
+++ src/views/UpdatePhone/UpdatePhone.js
@@ -1,98 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import { Form, Field } from 'react-final-form';
-import { adaptor } from 'components/form/utils';
-import { PhoneField } from 'components/form/fields';
-import { required, phone, composeValidators } from 'components/form/validation';
 import { FrameLayout as Layout } from 'views/common';
-
-const validate = accountPhone => values => {
-    const errors = {};
-    const { phone, newPhone } = values;
-
-    if (phone !== accountPhone) {
-        errors.phone = 'Введён неправильный номер телефона';
-    } else if (newPhone === phone) {
-        errors.newPhone = 'Введён старый номер телефона';
-    }
-
-    return errors;
-};
-
-// TODO: use final form hooks
+import Form from './Form';
 
 const UpdatePhone = ({ 
     loading, // TODO: disable ui
     accountPhone, 
     onSave 
-}) => {
-    return (
-        <Layout>
-            <Layout.Header>
-                Обновление номера телефона
-            </Layout.Header>
-            <Layout.Content>
-                <Form 
-                    validate={validate(accountPhone)}
-                    onSubmit={onSave}
-                >
-                    {({ handleSubmit, submitting }) => (
-                        <form onSubmit={handleSubmit}>
-                            <Grid 
-                                container 
-                                direction="column"
-                                spacing={3}
-                            >
-                                <Grid item xs={12}>
-                                    <Field
-                                        name="phone"
-                                        render={adaptor(PhoneField)}
-                                        variant="outlined"
-                                        label="Старый номер телефона"
-                                        fullWidth
-                                        validate={composeValidators(
-                                            required,
-                                            phone
-                                        )}
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Field
-                                        name="newPhone"
-                                        render={adaptor(PhoneField)}
-                                        variant="outlined"
-                                        label="Новый номер телефона"
-                                        fullWidth
-                                        validate={composeValidators(
-                                            required,
-                                            phone
-                                        )}
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Button 
-                                        color="primary"
-                                        variant="contained"
-                                        fullWidth
-                                        size="large"
-                                        type="submit"
-                                        disabled={submitting}
-                                    >
-                                        Сохранить
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </form>
-                    )}
-                </Form>
-            </Layout.Content>
-        </Layout>
-    );
-};
+}) => (
+    <Layout>
+        <Layout.Header>
+            Обновление номера телефона
+        </Layout.Header>
+        <Layout.Content>
+            <Form 
+                currentPhone={accountPhone}
+                onSubmit={onSave}
+            />
+        </Layout.Content>
+    </Layout>
+);
 
 UpdatePhone.propTypes = {
+    accountPhone: PropTypes.string,
     onSave: PropTypes.func,
 };
 
