203d0eb359b51cca24d40eeee8f7eac100cc7836
Yuri Rykov <rykov87yuri@gmail.com>
2020-04-04 12:35:43 +0300

    fixed npe exception

--- src/views/App/useCenteringFrameContent.js
+++ src/views/App/useCenteringFrameContent.js
@@ -2,20 +2,25 @@ import { useEffect } from 'react';
 import getSize from 'get-size';
 
 const centeringFrameContent = () => {
-    const frameContentEl = document.querySelector('main > section > article > *');
+    try {
+        const frameContentEl = document.querySelector('main > section > article > *');
 
-    const appWrapperHeight   = getSize('.app-layout-wrapper').innerHeight;
-    const appHeaderHeight    = getSize('.app-layout-wrapper > header').outerHeight;
-    const frameHeaderHeight  = getSize('main > section > h5').outerHeight;
-    const frameContentHeight = getSize('main > section > article > *').innerHeight;
+        const appWrapperHeight   = getSize('.app-layout-wrapper').innerHeight;
+        const appHeaderHeight    = getSize('.app-layout-wrapper > header').outerHeight;
+        const frameHeaderHeight  = getSize('main > section > h5').outerHeight;
+        const frameContentHeight = getSize('main > section > article > *').innerHeight;
 
-    const frameContentMarginTop = Math.max(
-        (appWrapperHeight - frameContentHeight) / 2 - 
-        (appHeaderHeight + frameHeaderHeight), 
-        0
-    );
-    
-    frameContentEl.style.marginTop = `${frameContentMarginTop}px`;
+        const frameContentMarginTop = Math.max(
+            (appWrapperHeight - frameContentHeight) / 2 - 
+            (appHeaderHeight + frameHeaderHeight), 
+            0
+        );
+        
+        frameContentEl.style.marginTop = `${frameContentMarginTop}px`;
+        
+    } catch (error) {
+        // some elements have not yet been drawn
+    }
 };
 
 export default () => {
